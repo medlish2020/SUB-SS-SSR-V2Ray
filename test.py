b877f03d6af6074803c6aa75b60ce05a504e6bd1
jaove <3158953@qq.com>
2020-11-17 13:57:10 +0800

    Daily Update !

--- test.py
+++ test.py
@@ -3,27 +3,35 @@ import sys
 
 configfilepath='configfile/'
 
-print('请粘贴SS及SSR账号内容，粘贴内容完毕后按回车后，按组合键ctrl+D保存提交内容:')
-
+print('\n请粘贴SS及SSR账号内容，粘贴内容完毕后按回车后，按组合键ctrl+D保存提交内容:')
 lines=sys.stdin.readlines()
-
 links_file=configfilepath+'host-SS.txt'
-fileObject = open(links_file,'w',encoding='UTF-8', errors='ignore')
 
-for line in lines:
-    fileObject.write(line)
-print('\nSS及SSR账号已保存！\n')
-fileObject.close()
-
+if lines:
+    fileObject = open(links_file,'w',encoding='UTF-8', errors='ignore')
+    for line in lines:
+        fileObject.write(line)
+    print('\nSS及SSR账号已保存！\n')
+    fileObject.close()
+else:
+    print('\n未输入SS及SSR账号，原SS及SSR账号内容未修改！\n')
 
 print('请粘贴V2Ray账号内容，粘贴内容完毕后按回车后，按组合键ctrl+D保存提交内容:')
 lines=sys.stdin.readlines()
-
 links_file=configfilepath+'host-V2Ray.txt'
 fileObject = open(links_file,'w',encoding='UTF-8', errors='ignore')
 
 for line in lines:
     fileObject.write(line)
+print('\nV2Ray账号已保存！\n')
+fileObject.close()
 
+print('请粘贴V2Ray账号内容，粘贴内容完毕后按回车后，按组合键ctrl+D保存提交内容:')
+lines=sys.stdin.readlines()
+links_file=configfilepath+'host-V2Ray.txt'
+fileObject = open(links_file,'w',encoding='UTF-8', errors='ignore')
+
+for line in lines:
+    fileObject.write(line)
 print('\nV2Ray账号已保存！\n')
 fileObject.close()
